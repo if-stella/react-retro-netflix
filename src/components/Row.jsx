1f8820dd85fa5f12d52b9038b0b62abf11992cff
Stella Raab <hallo@stellaraab.de>
2022-09-04 13:15:13 +0200

    Made unique rows slideable

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -13,13 +13,13 @@ const Row = ({title, fetchURL, rowID}) => {
   }, [fetchURL]);
 
   const slideLeft = () => {
-    var slider = document.getElementById('slider');
+    var slider = document.getElementById('slider' + rowID);
     slider.scrollLeft = slider.scrollLeft - 500;
   };
 
 
   const slideRight = () => {
-    var slider = document.getElementById('slider');
+    var slider = document.getElementById('slider' + rowID);
     slider.scrollLeft = slider.scrollLeft + 500;
   };
 
@@ -31,7 +31,7 @@ const Row = ({title, fetchURL, rowID}) => {
       onClick={slideLeft}
       className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 left-4 hidden group-hover:block"
       size={40}/>
-      <div id={'slider'} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+      <div id={'slider' + rowID} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
         {movies.map((item, id) => (
           <Movie key={id} item={item} />
         ))}
