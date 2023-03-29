a55dfbdf8ae05973ba9c87643b530329da9cdb66
Stella Raab <hallo@stellaraab.de>
2023-03-30 00:21:50 +0200

    Fixed by removing row 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
   return (
     <div className="home-container">
       <Main />
-      <Row rowID="1" title='Trending movies' fetchURL={requests.requestTrending}/>
       <Row rowID="2" title='Top rated' fetchURL={requests.requestTopRated}/>
       <Row rowID="3" title='Crime movies' fetchURL={requests.requestCrime}/>
       <Row rowID="4" title='Animated movies' fetchURL={requests.requestAni}/>
